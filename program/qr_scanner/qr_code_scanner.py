1cc4aa4470fb7da64e33f3194cbf2eb081ae8394
Hamid <hamid.martinez07@gmail.com>
2022-11-06 17:41:58 -0600

    Added camera control

--- program/qr_scanner/qr_code_scanner.py
+++ program/qr_scanner/qr_code_scanner.py
@@ -1,46 +1,28 @@
 import cv2
+from picamera import PiCamera
+from time import sleep
 
-# set up camera object called Cap which we will use to find OpenCV
-cap = cv2.VideoCapture(0)
+camera = PiCamera()
+camera.capture("/home/pi/Desktop/LARC/LARC/program/qr_scanner/qr_code.png")
 
-# QR code detection Method
-detector = cv2.QRCodeDetector()
+#qr = read_qr_code()
 
-#This creates an Infinite loop to keep your camera searching for data at all times
-while True:
-    
-    # Below is the method to get a image of the QR code
-    _, img = cap.read()
-    
-    # Below is the method to read the QR code by detetecting the bounding box coords and decoding the hidden QR data 
-    data, bbox, _ = detector.detectAndDecode(img)
-    
-    # This is how we get that Blue Box around our Data. This will draw one, and then Write the Data along with the top (Alter the numbers here to change the colour and thickness of the text)
-    if(bbox is not None):
-        for i in range(len(bbox)):
-            item1 = tuple(bbox[i][0])
-            item2 = tuple(bbox[i(1) % len(bbox)][0])
-            cv2.line(img, item1, item2, color=(255,0, 0), thickness=2)
+#print(qr)
 
-        cv2.putText(img, data, (int(bbox[0][0][0]), int(bbox[0][0][1]) - 10), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 250, 120), 2)
-        
-        #Below prints the found data to the below terminal (This we can easily expand on to capture the data to an Excel Sheet)
-        #You can also add content to before the pass. Say the system reads red it'll activate a Red LED and the same for Green.
-        if data:
-            print("data found: ", data)
-            if data == 'red':
-                pass
-            if data == 'green':
-                pass
-        
-            
-    # Below will display the live camera feed to the Desktop on Raspberry Pi OS preview
-    cv2.imshow("code detector", img)
+def read_qr_code(filename):
+    """Read an image and read the QR code.
     
-    #At any point if you want to stop the Code all you need to do is press 'q' on your keyboard
-    if(cv2.waitKey(1) == ord("q")):
-        break
+    Args:
+        filename (string): Path to file
     
-# When the code is stopped the below closes all the applications/windows that the above has created
-cap.release()
-cv2.destroyAllWindows()
+    Returns:
+        qr (string): Value from QR code
+    """
+    
+    try:
+        img = cv2.imread(filename)
+        detect = cv2.QRCodeDetector()
+        value, points, straight_qrcode = detect.detectAndDecode(img)
+        return value
+    except:
+        return "Error"
