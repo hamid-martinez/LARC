f3431be14d35576e6d601a632434cfd060717152
Hamid <hamid.martinez07@gmail.com>
2022-11-06 21:09:12 -0600

    made qr scanner class

--- program/qr_scanner/qr_code_scanner.py
+++ program/qr_scanner/qr_code_scanner.py
@@ -1,28 +1,29 @@
 import cv2
 from picamera import PiCamera
-from time import sleep
 
-def read_qr_code(filename):
-    """Read an image and read the QR code.
-    
-    Args:
-        filename (string): Path to file
-    
-    Returns:
-        qr (string): Value from QR code
-    """
-    
-    try:
-        img = cv2.imread(filename)
-        detect = cv2.QRCodeDetector()
-        value, points, straight_qrcode = detect.detectAndDecode(img)
-        return value
-    except:
-        return "Error"
+# Suggested location for qr_code: "/home/pi/Desktop/LARC/LARC/program/qr_scanner/qr_code.png"
 
-camera = PiCamera()
-camera.capture("/home/pi/Desktop/LARC/LARC/program/qr_scanner/qr_code.png")
+class qr_code_scanner():
 
-qr = read_qr_code("/home/pi/Desktop/LARC/LARC/program/qr_scanner/qr_code.png")
+    def __init__(self, file_location):
+        
+        self.file_location = file_location
 
-print(qr)
+    def take_picture(self):
+
+        camera = PiCamera()
+        camera.capture(self.file_location)
+
+        return f"Picture taken at {self.file_location}"
+
+    def read_qr_code(self):
+
+        try:
+            img = cv2.imread(self.file_location)
+            detect = cv2.QRCodeDetector()
+            value, points, straight_qrcode = detect.detectAndDecode(img)
+
+            return value
+
+        except:
+            return "ERROR: No QR-code detected"
