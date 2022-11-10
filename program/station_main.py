e25a48a59e067a0184dbd4467eb41c0cb64aa31b
Hamid <hamid.martinez07@gmail.com>
2022-11-10 00:07:32 -0600

    added conveyor control with sensor

--- program/station_main.py
+++ program/station_main.py
@@ -1,28 +1,43 @@
-from pi_gpio_control.gpio_control import Gpio_Control
+import RPi.GPIO as GPIO
+from google_sheet_comm.sheet_comm import Sheet_Comm
+from time import sleep
 
-pin_mode = "BOARD"
-pin_type_motor = "OUT"
-pin_type_sensor = "IN"
+sheet = "Inventory"
+work_sheet = "Products"
+sensor_state_sheet = [2,4] # Location at google sheet [row,column]
 
-# Motor control:
-motor_dir = Gpio_Control(pin_mode, pin_type_motor, [16])
-motor_pwm = Gpio_Control(pin_mode, pin_type_motor, [12])
+# Sheet communication
+sheet = Sheet_Comm(sheet, work_sheet)
 
-# Sensor
-sensor = Gpio_Control(pin_mode, pin_type_sensor, [37])
+enable = 11
+dir = 16
+pwm = 12
+sensor = 37
 
-continue_code = True
+GPIO.setwarnings(False) # Avoid warnings messages at start
+GPIO.setmode(GPIO.BOARD)
 
-while continue_code == True:
+GPIO.setup(dir, GPIO.OUT, initial = GPIO.LOW)
+GPIO.setup(pwm, GPIO.OUT, initial = GPIO.LOW)
+GPIO.setup(enable, GPIO.OUT, initial = GPIO.HIGH)
+GPIO.setup(sensor, GPIO.IN)
 
-    motor_dir.state_on()
-    value = sensor.read_pin()
+motor = GPIO.PWM(pwm,100)
+motor.start(0)
 
-    while value[0] == 0:
+while True:
 
-        value = sensor.read_pin()
-        motor_pwm.set_pwm(50, 20, False)
-        print(value)
+    #sensor_state = sensor.read_pin() #[0]
+    sensor_state = GPIO.input(sensor)
+    sleep(0.5)
 
-    motor_pwm.set_pwm(50, 20, True)
+    if sensor_state == 0:
+        GPIO.output(dir, GPIO.HIGH)
+        motor.ChangeDutyCycle(15)
+        GPIO.output(enable, GPIO.LOW)
+        sleep(0.5)
+
+    if sensor_state == 1:
+        GPIO.output(enable, GPIO.HIGH)
+        motor.stop()
 
